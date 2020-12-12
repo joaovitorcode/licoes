763b3eca47560ac7d3cff6ceaea61c26868b8b05
J Vitor F <joaovitor.jobs2001@gmail.com>
2020-12-12 16:22:42 -0300

    bin2Dec v1.0

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Lista de lições
 
-1. Calculadora Versão 1.0
+1. Calculadora V1.0
+2. Bin2Dec V1.0
---
+++ bin2Dec/bin2Dec.js
@@ -0,0 +1,30 @@
+let pOutput = document.querySelector('output p');
+let input = document.querySelector('.input input');
+let button = document.querySelector('.input button');
+
+converte(input);
+
+function converte(numBin) {
+
+    button.addEventListener('click', (event) => {
+
+        event.preventDefault();
+
+        // Separa cada caractere da string transformando-o num item de um array
+        let array = numBin.value.split('');
+        let novoArray = [];
+        let base = 2;
+        
+        array.forEach((elemento, indice) => {
+            
+            let expoente = (array.length - 1) - indice;
+            novoArray.push((base ** expoente) * Number(elemento));
+        });
+        
+        let resultado = novoArray.reduce((soma, item) => {
+
+            return soma + item;
+        })
+        console.log(resultado);
+    })
+}
