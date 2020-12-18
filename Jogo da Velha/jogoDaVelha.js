afb67a67a3107f2c8dbd114f75f28ec0cdf38eba
J Vitor F <joaovitor.jobs2001@gmail.com>
2020-12-18 16:32:17 -0300

    alteração do nome da classe 'quem-joga' para 'info'

--- Jogo da Velha/jogoDaVelha.js
+++ Jogo da Velha/jogoDaVelha.js
@@ -1,6 +1,6 @@
 let quadradoMaior = document.querySelector('.quadrado-maior');
 let quadradoMenor = document.querySelectorAll('.quadrado-menor');
-let quemJoga = document.querySelector('.quem-joga');
+let info = document.querySelector('.info');
 let trocaJogador = true;
 
 quadradoMaior.addEventListener('mousedown', (event) => {
@@ -8,12 +8,12 @@ quadradoMaior.addEventListener('mousedown', (event) => {
     if (trocaJogador) {
 
         event.target.textContent = 'X';
-        quemJoga.textContent = 'Vez de "O" jogar';
+        info.textContent = 'Vez de "O" jogar';
         trocaJogador = false;
     } else {
 
         event.target.textContent = 'O';
-        quemJoga.textContent = 'Vez de "X" jogar';
+        info.textContent = 'Vez de "X" jogar';
         trocaJogador = true;
-    } 
+    }
 });
